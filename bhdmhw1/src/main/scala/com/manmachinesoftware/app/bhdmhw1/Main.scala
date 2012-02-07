855183a5cfc72366c78ca1e3b4aa127164b678c9
Javier Rosa <javirosa@eecs.berkeley.edu>
2012-02-06 16:58:52 -0800

    Prepping for creating separate classifier

--- bhdmhw1/src/main/scala/com/manmachinesoftware/app/bhdmhw1/Main.scala
+++ bhdmhw1/src/main/scala/com/manmachinesoftware/app/bhdmhw1/Main.scala
@@ -2,14 +2,15 @@ package com.manmachinesoftware.app.bhdmhw1
 
 import java.util.ArrayList
 import java.io.File
-import scala.io.Source
-import scala.collection.immutable
-import scala.collection.mutable.HashMap
-import scala.collection.mutable.Map
+import io.Source
+import collection.immutable
+import collection.mutable.HashMap
+import collection.mutable.Map
+import collection.mutable.ListBuffer
 import scalala.tensor.counters.Counters.DefaultIntCounter
 import scalanlp.data.{LabeledDocument}
-import scala.collection.mutable.ListBuffer
 import scalala.tensor.sparse.SparseVector
+import util.Random
 
 
 object Main
@@ -21,6 +22,7 @@ object Main
     val mysql = new java.io.File(getClass().getResource("/stopwords/mysqlStop.txt").getFile())
     val ranknlAll = new java.io.File(getClass().getResource("/stopwords/ranknlAllStop.txt").getFile())
     val ranknl = new java.io.File(getClass().getResource("/stopwords/ranknlStop.txt").getFile())
+    var xFold = 10
 
     //Parameters
     val useStopWords = mysql
@@ -43,6 +45,9 @@ object Main
     //Method which takes the fneg and fpos sets and divides them into sets of size 10
     // for each subset run the algorithm
 
+    def runClassifier(corpus:List[LabeledDocument[Double,String]], sW:Array[String],useStemmer:Boolean ) = {
+
+    }
     def getData() = {
         //Open resource directories for +ve and -ve
         val fnegPath = new java.io.File(getClass().getResource("/polarityData/neg").getFile())
@@ -59,7 +64,6 @@ object Main
             val seq = parseFile(new File(fposPath,review),sW = stopWords, stem=useStemmer)
             corpus += (new LabeledDocument(review,POS,immutable.HashMap((featureBody,seq))))
         }
-        println(corpus.head)
 
         //Build dictionary and index
         var dict = new HashMap[String,Int]()//{ override def default(key:String) = 0}
@@ -79,6 +83,24 @@ object Main
             i = i + 1
         }
 
+        //Generate Random sets from the corpus
+        val sets = List(Random.shuffle(corpus).sliding(corpus.size/xFold))
+
+        //For each set train on everything else, but that set
+        for (t <- 0 until sets.length) {
+            //Train against the rest
+
+            //Run the query
+
+            //Compute F1 scores
+
+        }
+
+        //Plot F1 scores
+
+        //Print distinguishing words
+
+        //Compare against a bernoulli bayes without stemming or stopwords
     }
 
     def encode(dict:Map[String,Double],wToIdx:Map[String,Int]):SparseVector = {
